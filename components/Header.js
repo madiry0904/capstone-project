ec95de946f666fda92f9329ad3545383e70ed8b6
Madi Ryan <madi.ryan92@gmail.com>
2020-01-22 19:36:55 -0600

    add links for arrays

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default () => `
 <header id="header" class="class ="title-nav-wrapper">
-  <h1>Stress Relief Made Simple</h1>
+  <h1>Stress Made Simple</h1>
 
   </header>
   `;
--- components/views/Quiz.js
+++ components/views/Quiz.js
@@ -19,7 +19,7 @@ export default st => `
     <label for ="time">Please select the amount of time you have</label>
     <select id = "time" name= "number">
       <option value = "30"> 30 minutes or less</option>
-      <option value ="45">45 minutes</option>
+      <option value ="45">45 minutes </option>
       <option value = "60">60 minutes or more</option>
     </select>
   </div>
